8bcca43f0d3071bc02a670ca84eec3b711d0f031
ayoubessafi <57940108+ayoubessafi@users.noreply.github.com>
2020-12-01 14:52:57 +0100

    Ajoutez des fichiers projet.

---
+++ atelier 1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace atelier_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double pHT;
+            double tva;
+            double pttc;
+
+            Console.WriteLine("Veuillez saisir un produit");
+            string pdt = Console.ReadLine();
+
+            Console.WriteLine("Veuillez saisir un prix hors taxe");
+            pHT = (double.Parse(Console.ReadLine()));
+            Console.WriteLine(" ");
+
+            Console.WriteLine("Veuillez saisir le taux de la TVA: ");
+            tva = (double.Parse(Console.ReadLine()));
+            Console.WriteLine(" ");
+
+            pttc = pHT * (1+ (tva/100));
+            Console.WriteLine(" Le prix total est de: " + pttc);
+            Console.WriteLine(" ");
+
+
+
+        }
+    }
+}
+
