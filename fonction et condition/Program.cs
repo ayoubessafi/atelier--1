1df6eafe356eb5e60f0b7a10f531b88131489dee
ayoubessafi <57940108+ayoubessafi@users.noreply.github.com>
2020-12-02 14:05:08 +0100

    calculatrice

--- fonction et condition/Program.cs
+++ fonction et condition/Program.cs
@@ -6,15 +6,73 @@ namespace fonction_et_condition
     {
         static void Main(string[] args)
         {
-            int a = 2;
-            int b = 4;
-            while ( a < 15)
+            Console.WriteLine("Entrez l'action à effectuer");
+            Console.WriteLine("Presse a pour addition");
+            Console.WriteLine("Presse s pour la soustraction ");
+            Console.WriteLine("Presse m pour la multiplication" );
+            Console.WriteLine("Presse d pour la division");
+            string action = Console.ReadLine();
+            Console.WriteLine("tapis 1ére nombre");
+            int input_1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("tapis 2éme nombre");
+            int input_2 = Convert.ToInt32(Console.ReadLine());
+            int result = 0;
+            switch (action)
             {
-                int c = a * b;
-                Console.WriteLine(c);
-                a++;
+                case "a":
+                    {
+                        result = Addition(input_1, input_2);
+                        break;
+                    }
+                case "s":
+                    {
+                        result = Subtraction(input_1, input_2);
+                        break;
+                    }
+                case "m":
+                    {
+                        result = Multiplication(input_1, input_2);
+                        break;
+                    }
+                case "d":
+                    {
+                        result = Division(input_1, input_2);
+                        break;
+                    }
+                default:
+                    Console.WriteLine("Wrong action!! try again");
+                    break;
             }
-
+            Console.WriteLine("The result is {0}",result);
+            Console.WriteLine("presse q pour arrêter");
+            if (Console.ReadLine() == "q")
+            {
+                Environment.Exit(0);
+            }
+        }
+        //Addition  
+        public static int Addition(int input_1, int input_2)
+        {
+            int result = input_1 + input_2;
+            return result;
+        }
+        //Substraction  
+        public static int Subtraction(int input_1, int input_2)
+        {
+            int result = input_1 + input_2;
+            return result;
+        }
+        //Multiplication  
+        public static int Multiplication(int input_1, int input_2)
+        {
+            int result = input_1 + input_2;
+            return result;
+        }
+        //Division  
+        public static int Division(int input_1, int input_2)
+        {
+            int result = input_1 + input_2;
+            return result;
         }
     }
 }
