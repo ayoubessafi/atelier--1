ce4000750cdf39c45963444a8ecb942cc59bdf60
ayoubessafi <57940108+ayoubessafi@users.noreply.github.com>
2020-12-02 09:32:19 +0100

    test

---
+++ fonction et condition/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace fonction_et_condition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 2;
+            int b = 4;
+            while ( a < 15)
+            {
+                int c = a * b;
+                Console.WriteLine(c);
+                a++;
+            }
+
+        }
+    }
+}
