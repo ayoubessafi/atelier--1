da9bf5b49afaf79fd980efe7774da61b69908461
ayoubessafi <57940108+ayoubessafi@users.noreply.github.com>
2020-12-03 19:24:16 +0100

    modefication

--- atelier 1/Program.cs
+++ atelier 1/Program.cs
@@ -15,15 +15,15 @@ namespace atelier_1
 
             Console.WriteLine("Veuillez saisir un prix hors taxe");
             pHT = (double.Parse(Console.ReadLine()));
-            Console.WriteLine(" ");
+            
 
             Console.WriteLine("Veuillez saisir le taux de la TVA: ");
             tva = (double.Parse(Console.ReadLine()));
-            Console.WriteLine(" ");
+            
 
             pttc = pHT * (1+ (tva/100));
-            Console.WriteLine(" Le prix total est de: " + pttc);
-            Console.WriteLine(" ");
+            Console.WriteLine("Le prix total est de: " + pttc);
+            
 
 
 
